582615f32f732a9e1cb61300b1863aa4f620b604
Anandkumar K <an81195@gmail.com>
2025-07-20 22:03:52 -0700

    logger update

--- src/main/java/com/ak/http_request/HelloController.java
+++ src/main/java/com/ak/http_request/HelloController.java
@@ -1,13 +1,19 @@
 package com.ak.http_request;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
+    // Create a logger instance
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String sayHello(@RequestParam(defaultValue = "World") String name) {
+        logger.info("Received request to /hello with name: {}", name);
         return "Hello, " + name + "!";
     }
 }
